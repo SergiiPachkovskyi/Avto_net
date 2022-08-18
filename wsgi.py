5bb6fc881d266ca39049a8aad3455932d91c520f
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-08-18 13:53:35 +0300

    add wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ from api import create_app
 from api.config.config import config_dict
 
 
+app = create_app(config_dict['prod'])
+
 if __name__ == '__main__':
-    app = create_app(config_dict['prod'])
     app.run()
