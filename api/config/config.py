dfb1e6e6e6860d66061cbac51a32bfc319b36b5e
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-08-18 11:00:39 +0300

    add wsgi

--- api/config/config.py
+++ api/config/config.py
@@ -1,9 +1,11 @@
-import os
 from datetime import timedelta
 
 from decouple import config
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# uri = config("DATABASE_URL")  # or other relevant config var
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
 
 
 class Config:
@@ -14,7 +16,6 @@ class Config:
 
 
 class DevConfig(Config):
-    # DEBUG = config('DEBUG', cast=bool)
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://root:Saint32366n!@localhost/avto_net'
@@ -28,7 +29,10 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_DATABASE_URI = 'postgres://saintdaemon:saint32366n!@localhost/avto_net'
+    DEBUG = config('DEBUG', cast=bool)
 
 
 config_dict = {
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from api import create_app
 
+
 if __name__ == '__main__':
     dev_app = create_app()
     dev_app.run()
